6f24b46b3e2b0ef3fc8d1a00ebf73bce65da62ec
Dearborn42 <amurf26@outlook.com>
2023-04-09 22:24:13 -0700

    Added icons to navbar

--- LegitAppName/app/nav.js
+++ LegitAppName/app/nav.js
@@ -2,15 +2,36 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Emails from  './Components/email';
 import Home from './Components/home';
 import Message from './Components/message';
+import { Ionicons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons'; 
 
 const Tab = createBottomTabNavigator();
 
 export default function MyTabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" options={{headerShown: false}} component={Home} />
-      <Tab.Screen name="Email" options={{headerShown: false}} component={Emails} />
-      <Tab.Screen name="message" options={{headerShown: false}} component={Message} />
+      <Tab.Screen 
+        name=" " 
+        options={{
+          headerShown: false,         
+          tabBarIcon: () => (<Ionicons name="home" color={'#185708'} size={30} />)
+        }} 
+        component={Home}
+      />
+      <Tab.Screen 
+        name="  " 
+        options={
+          {headerShown: false, 
+          tabBarIcon: () => (<Ionicons name="mail-sharp" size={35} color='#185708' />)
+        }} 
+        component={Emails} />
+      <Tab.Screen 
+        name="   " 
+        options={
+          {headerShown: false,
+            tabBarIcon: () => (<Entypo name="message" size={35} color="#185708" />)
+        }} 
+        component={Message} />
     </Tab.Navigator>
   );
 }
