fb552b2f6c636f7ae523427ed84b683ae44de2aa
Dearborn42 <amurf26@outlook.com>
2023-04-12 16:50:51 -0700

    Added content to page

--- LegitAppName/app/screens/data.js
+++ LegitAppName/app/screens/data.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import styles from '../styles/_emailLayout';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 
 export default function Data({ navigation }) {
   return (
     <View style={styles.components}>
+      <Text>Hello</Text>
     </View>
   );
 }
