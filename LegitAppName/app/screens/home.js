67f212760282ddf4e858317a6ffd4d6c3e035dbf
Dearborn42 <amurf26@outlook.com>
2023-10-02 00:30:14 -0700

    Revert "added options to home page"
    This reverts commit 6c49f901471b81c7c33dae72c3ff5a50ffee253e.

--- LegitAppName/app/screens/home.js
+++ LegitAppName/app/screens/home.js
@@ -1,13 +1,13 @@
 import { WebView } from 'react-native-webview';
 import { View } from 'react-native';
-const home = require("../../html/home.html");
+const test = require("../../html/test.html");
 
 
 export default function Home() {
   return (
     <View style={{flex: 1}}>
       <WebView
-        source={home}
+        source={test}
       />
     </View>
   );
